4004336d81bc0a7b374eb1e53db9f68d1de174cc
dennis <dennisliu010906@gmail.com>
2024-10-24 01:27:33 -0400

    changed home page, added color variables

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -11,6 +11,7 @@ const config: Config = {
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
+        'text-color': "#333333"
       },
     },
   },
