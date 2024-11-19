690a6bf1a46e399f42e1892732b26d3cd2593817
Shawne Laquindanum <shawnebenedict@Outlook.com>
2024-11-18 22:01:38 -0500

    Added new componenets to NavBar

--- src/app/_components/navbar.tsx
+++ src/app/_components/navbar.tsx
@@ -13,21 +13,22 @@ export default function NavBar() {
               width={90}
               height={50}
             ></Image>
-            <h1 className= "font-bold text-white"> DormPop </h1>
+            <h1 className="font-bold text-white text-xl">
+              <a href=""> DormPop </a>
+            </h1>
           </div>
           <div>
             <input
-              className=" font-semibold w-[500px] pl-3 p-1 rounded-full border-4 border-black focus:outline-none "
+              className=" font-semibold w-[500px] pl-3 p-1 rounded-full border-2 border-black focus:outline-none "
               type="text"
               placeholder="Search..."
-
             />
           </div>
-          <ul className="p-7 space-x-5 text-white ">
-            <a href=""> About </a>
-            <a href=""> Contact </a>
-            <a href=""> Home </a>
-            <a href=""> Shop </a>
+          <ul className="p-7 space-x-5 text-white text-lg">
+            <a href="" className="hover:underline"> Wishlist </a>
+            <a href="" className="hover:underline"> Cart </a>
+            <a href="" className="hover:underline"> Log In </a>
+            <a href="" className="hover:underline"> Sign up </a>
           </ul>
         </nav>
       </header>
